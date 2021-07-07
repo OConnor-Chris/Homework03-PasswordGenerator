42f4f0459298b9dba7a998cbb7750e15f6d1b255
Chris O'Connor <chrisgocon@gmail.com>
2021-07-06 20:13:31 -0500

    Added password length alerts

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,19 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+  var passwordLengthChoice = prompt("Choose a password length between 8-128 characters.  Don't be weird.");
+  passwordLengthChoice = parseInt(passwordLengthChoice);
+  if (passwordLengthChoice < 8) {
+    alert("Must have at least 8 characters.  We talked about this.");
+  }
+  if (passwordLengthChoice > 128) {
+    alert("Must have fewer than 128 characters.  What's your deal?")
+  }
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
+//
 function getLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
